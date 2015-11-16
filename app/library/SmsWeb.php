f768c150d5e6ef27edcf78076e6e51d25c66cdfb
Rolly Maulana Awangga <rolly@awang.ga>
2015-11-16 14:56:10 +0700

    rapihkan

--- app/library/SmsWeb.php
+++ app/library/SmsWeb.php
@@ -19,7 +19,7 @@ class SmsWeb
 	    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 	    curl_setopt($ch,CURLOPT_HEADER, false); 
 	    curl_setopt($ch, CURLOPT_POST, count($postData));
-	        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
 
 	    $output=curl_exec($ch);
 
