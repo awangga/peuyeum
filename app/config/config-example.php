fb0a33166e77b99cc7ddfcbd77d450aa906ba656
Rolly Maulana Awangga <rolly@awang.ga>
2016-02-24 15:21:41 +0700

    add config example

--- app/config/config-example.php
+++ app/config/config-example.php
@@ -19,11 +19,6 @@ return new \Phalcon\Config(array(
         'pluginsDir'     => APP_PATH . '/app/plugins/',
         'libraryDir'     => APP_PATH . '/app/library/',
         'cacheDir'       => APP_PATH . '/app/cache/',
-        'baseUri'        => '/',
-    ),
-	'peuyeum' => array(
-        'adapter' => 'mongo',
-        'collectionManager'      => 'collectionManager',
-        'dbname'  => 'peuyeum',
+        'baseUri'        => '/peuyeum/',
     )
 ));
